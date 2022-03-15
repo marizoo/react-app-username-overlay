664524e6d8dd1f3618da20f36826ff94da50872e
marizoo <marizoo@meow.woof>
2022-03-15 21:57:12 +0700

    added react portals from react-dom

--- src/components/errorModal/ErrorModal.jsx
+++ src/components/errorModal/ErrorModal.jsx
@@ -2,25 +2,49 @@ import React from "react";
 import Button from "../UI/button/Button";
 import Card from "../UI/card/Card";
 import classes from "./errorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+    return (
+        <div
+            onClick={props.onCloseErrorModal}
+            className={classes.backdrop}
+        ></div>
+    );
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onCloseErrorModal}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
 
 const ErrorModal = (props) => {
     return (
         <>
-            <div
-                onClick={props.onCloseErrorModal}
-                className={classes.backdrop}
-            ></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onCloseErrorModal}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <Backdrop onCloseErrorModal={props.onCloseErrorModal} />,
+                document.getElementById("backdrop-root")
+            )}
+
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    onCloseErrorModal={props.onCloseErrorModal}
+                    title={props.title}
+                    message={props.message}
+                />,
+                document.getElementById("overlay-root")
+            )}
         </>
     );
 };
